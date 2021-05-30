04ca9b1350d00c675c29f3b82a9ab4965ee6e0b4
Douglas <douglas@mediushealth.org>
2021-05-30 21:15:22 +1000

    server

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const app = express();
 const publicPath = path.join(__dirname, "build");
 const port = process.env.PORT || 5000;
 
-app.use(express.static(publicPath));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
+console.log(publicPath);
+
+if (process.env.NODE_ENV === "produciton") {
+  app.use(express.static(publicPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log("Server is up!");
